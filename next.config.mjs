ea2e2c05ebf11a782cd25620d413c006b6a757f3
Saviovd <savio.pinheiroal@gmail.com>
2024-05-28 18:23:14 -0400

    add: app settings

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+   reactStrictMode: true,
+   eslint: {
+      ignoreDuringBuilds: true,
+   },
+   images: {
+      domains: ['media.licdn.com', 'www.google.com', 'github.com', 'abrudz.github.io', 'cdn.icon-icons.com', 'static-00.iconduck.com','raw.githubusercontent.com', 'w7.pngwing.com'],
+   },
+   i18n: {
+      locales: ['en', 'pt', 'es'],
+      defaultLocale: 'pt',
+   },
+};
 
-export default nextConfig;
+module.exports = nextConfig;
