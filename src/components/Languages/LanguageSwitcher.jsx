bb2369e266de76769c8a45925ec58fc7f2d05e2e
Saviovd <savio.pinheiroal@gmail.com>
2024-05-28 20:51:31 -0400

    add/ 10 lang support

--- src/components/Languages/LanguageSwitcher.jsx
+++ src/components/Languages/LanguageSwitcher.jsx
@@ -6,14 +6,34 @@ import Link from 'next/link';
 export default function LanguageSwitcher() {
    const pathname = usePathname();
    const searchParams = useSearchParams();
-   const locales = ['en', 'pt'];
+   const locales = ['en', 'pt', 'zh', 'es', 'hi', 'fr', 'ar', 'bn', 'ru', 'id'];
 
    return (
       <div>
          {locales.map((loc) => (
             <Link key={loc} href={loc} locale={loc} passHref legacyBehavior>
                <a style={{ marginRight: 10 }}>
-                  {loc === 'en' ? 'English - US' : 'Portuguese - BR'}
+                  {loc === 'en'
+                     ? 'English - US'
+                     : loc === 'pt'
+                     ? 'Portuguese - BR'
+                     : loc === 'zh'
+                     ? 'Chinese (Mandarin)'
+                     : loc === 'es'
+                     ? 'Spanish (Spain)'
+                     : loc === 'hi'
+                     ? 'Hindi (India)'
+                     : loc === 'fr'
+                     ? 'French (France)'
+                     : loc === 'ar'
+                     ? 'Arabic (Arab Countries)'
+                     : loc === 'bn'
+                     ? 'Bengali (Bangladesh)'
+                     : loc === 'ru'
+                     ? 'Russian (Russia)'
+                     : loc === 'id'
+                     ? 'Indonesian (Indonesia)'
+                     : 'Unknown Language'}
                </a>
             </Link>
          ))}
--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,7 @@ import createMiddleware from 'next-intl/middleware';
 
 export default createMiddleware({
    // A list of all locales that are supported
-   locales: ['en', 'pt'],
+   locales: ['en', 'pt', 'zh', 'es', 'hi', 'fr', 'ar', 'bn', 'ru', 'id'],
 
    // Used when no locale matches
    defaultLocale: 'en',
@@ -10,5 +10,5 @@ export default createMiddleware({
 
 export const config = {
    // Match only internationalized pathnames
-   matcher: ['/', '/(pt|en)/:path*'],
+   matcher: ['/', '/(en|pt|zh|es|hi|fr|ar|bn|ru|id)/:path*'],
 };
