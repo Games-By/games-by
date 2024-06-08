f00ae854a7ec1bfd1643dd9564db2998cf95fd05
Saviovd <savio.pinheiroal@gmail.com>
2024-06-08 17:49:42 -0400

    add: prevent /profile if user is not logged

--- src/app/[locale]/profile/page.jsx
+++ src/app/[locale]/profile/page.jsx
@@ -1,16 +1,28 @@
-'use client'
+'use client';
 import GlobalStyle from '@/Styles/globals';
-import React from 'react';
+import React, { useEffect } from 'react';
 import Header from '@/components/Header/Header';
 import { useRouter } from '../../../../navigation.ts';
 
 const Profile = () => {
-   const router = useRouter()
+   const router = useRouter();
+
+   const VerifyLogin = async () => {
+      const authToken = localStorage.getItem('authToken');
+      if (!authToken) {
+         router.replace('/');
+      }
+   };
+
    const handleLogout = () => {
       localStorage.removeItem('authToken');
 
       router.replace('/login');
    };
+
+   useEffect(() => {
+      VerifyLogin()
+   }, []);
 
    return (
       <>
