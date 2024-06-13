783e01c13a6a48016419e08d6206678af5fdc651
Saviovd <savio.pinheiroal@gmail.com>
2024-06-12 21:10:42 -0400

    fix: image contain

--- src/components/ProfileThumb/ProfileThumbStyles.js
+++ src/components/ProfileThumb/ProfileThumbStyles.js
@@ -14,7 +14,7 @@ export const ProfileThumbStyles = styled.div`
       border-radius: 50%;
       width: 100%;
       height: 100%;
-      object-fit: cover;
+      object-fit: contain;
    }
 
    .profile-icon {
