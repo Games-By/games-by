a89c4a29451558e62627f7454f6786c07251fee9
Saviovd <savio.pinheiroal@gmail.com>
2024-06-17 16:19:20 -0400

    fix: crossOrigin error

--- src/app/[locale]/layout.js
+++ src/app/[locale]/layout.js
@@ -14,7 +14,7 @@ export default async function LocaleLayout({ children, params: { locale } }) {
             <link
                rel='preconnect'
                href='https://fonts.gstatic.com'
-               crossorigin
+               crossOrigin='true'
             />
             <link
                href='https://fonts.googleapis.com/css2?family=Reddit+Sans:ital,wght@0,200..900;1,200..900&display=swap'
