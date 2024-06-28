b8f56a6c7ecf6af01ac9ce92103c34c30f185ac4
Saviovd <savio.pinheiroal@gmail.com>
2024-06-28 15:58:24 -0400

    fix: animation transition

--- src/components/Button/ButtonStyles.js
+++ src/components/Button/ButtonStyles.js
@@ -15,7 +15,7 @@ export const ButtonStyles = styled.button`
    padding: 0.8rem 3.5rem;
    display: flex;
    align-items: center;
-
+   transition: 0.5s;
    color: ${(props) => props.color || 'rgba(var(--light))'};
    border: 1px solid ${(props) => props.color || 'rgba(var(--light))'};
 
@@ -28,8 +28,7 @@ export const ButtonStyles = styled.button`
    }
 
    &:hover {
-      transition: 0.2s;
-      transform: scale(1.01);
+      transform: scale(1.03);
       color: ${(props) => props.hoverColor || 'rgba(var(--light))'};
       border: 1px solid ${(props) => props.hoverColor || 'rgba(var(--light))'};
       svg {
