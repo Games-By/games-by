f7256b2b6624674e7809986d9813675534bef43e
tiagocostta <tiagoct884@gmail.com>
2024-07-01 18:21:21 -0300

    Add: components DiscountPrice

---
+++ src/components/DiscountPrice/DiscountPriceStyles.js
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+export const DiscountPriceStyles = styled.p`
+   position: relative;
+   max-width: 8rem;
+   display: flex;
+   justify-content: space-evenly;
+   align-items: center;
+   font-size: 1.35rem;
+   font-weight: 200;
+
+   color: rgba(var(--red));
+   &::after {
+      content: '';
+      height: 1px;
+      width: 100%;
+      background-color: rgba(var(--red),0.75);
+      top: 50%;
+      left: 0;
+      position: absolute;
+      transform: rotate(-5deg);
+   }
+`;
