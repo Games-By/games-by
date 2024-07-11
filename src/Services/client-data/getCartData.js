49b3d6a2e31baea315dc897008bda49a52f6ee98
Saviovd <savio.pinheiroal@gmail.com>
2024-07-10 23:33:12 -0400

    add: get cart list

---
+++ src/Services/client-data/getCartData.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+export const getCart = async () => {
+   const user = localStorage.getItem('user');
+   if (user) {
+      const formatedUser = JSON.parse(user);
+      const userId = formatedUser._id;
+      const response = await axios.get(
+         `${process.env.NEXT_PUBLIC_SERVER_URL}/${userId}/cart`
+      );
+      return response.data.cart;
+   } else {
+      return 0;
+   }
+};
