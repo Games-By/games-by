2e0a5e3ae0e0537864b94f5042614349a036d5c8
Saviovd <savio.pinheiroal@gmail.com>
2024-07-12 19:50:10 -0400

    add: animate dropdowns in header

--- src/components/Dropdown/DropdownStyles.js
+++ src/components/Dropdown/DropdownStyles.js
@@ -1,66 +1,72 @@
+import { motion } from 'framer-motion';
 import styled from 'styled-components';
 
-export const DropdownStyles = styled.div`
+export const DropdownStyles = styled(motion.div)`
    display: flex;
    flex-direction: column;
    justify-content: center;
    align-items: center;
    gap: 1.5rem;
-   padding: 1rem 0;
-   background-color: rgba(var(--dark));
+   padding: 1rem 0 2rem;
+   background-color: rgba(var(--dark), .9);
    position: absolute;
    top: 8rem;
    right: 5rem;
    z-index: 1;
-   width: 15rem;
+   min-width: 25rem;
+   width: 25%;
    border-radius: 0 0 2rem 2rem;
-   border: 1px solid rgba(var(--purple-1));
+   border: 1px solid rgba(var(--cyan));
    border-top: none;
    color: rgba(var(--light));
-   transition: 0.5s;
    z-index: 2;
 
    &::before {
       content: '';
-      height: 3rem;
-      width: 15rem;
+      height: 4rem;
+      width: 20rem;
       background-color: transparent;
       position: fixed;
-      top: 6rem;
+      top: 5rem;
    }
 
    .option {
       text-align: center;
-      width: 10rem;
+      width: 100%;
       margin: 0 auto;
       color: rgba(var(--light));
-      font-size: 1.5rem;
+      font-size: 1.8rem;
       display: flex;
       align-items: center;
       justify-content: center;
+      font-weight: 300;
+      white-space: nowrap;
+      letter-spacing: 1px;
 
       svg {
          fill: rgba(var(--light));
-         margin-left: 1rem;
+         margin-left: .5rem;
+         padding-left: .5rem;
+         width: 1.8rem;
+         border-left: 1px solid rgba(var(--light));
       }
 
       &:hover {
-         color: rgba(var(--purple-2), 0.8);
+         color: rgba(var(--cyan), 0.8);
          svg {
-            fill: rgba(var(--purple-2));
+            fill: rgba(var(--cyan));
          }
       }
    }
 
    @media screen {
       @media (max-width: 660px) {
-         width: 70vw;
+         width: 0;
          max-width: 40rem;
          border-radius: 0 0 0 2rem;
          right: 0;
          border-right: none;
-         top: 5.9rem;
-         width: 0;
+         top: 6rem;
 
          &::before {
             display: none;
