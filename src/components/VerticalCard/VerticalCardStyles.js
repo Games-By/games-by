363b231b7a6b1539493465fdf67270d275c6a495
Saviovd <savio.pinheiroal@gmail.com>
2024-07-18 14:49:44 -0400

    add: vertical card responsiviness

--- src/components/VerticalCard/VerticalCardStyles.js
+++ src/components/VerticalCard/VerticalCardStyles.js
@@ -1,92 +1,318 @@
 import styled from 'styled-components';
 
 export const VarticalCardStyles = styled.div`
-   border-radius: 1rem;
-   max-width: 29rem;
-   background-color: rgba(var(--dark));
-   margin: 2rem auto;
-   height: 100%;
-   cursor: pointer;
+   height: auto;
+   max-width: 30rem;
+   width: 25%;
+   display: flex;
+   flex-direction: column;
+   align-items: center;
+   justify-content: center;
+
    .cover {
+      height: 60%;
+      width: 100%;
       border-radius: 1rem 1rem 0 0;
-   }
-   .infos {
-      border-radius: 0 0 1rem 1rem;
-      padding: 1rem;
-      background: linear-gradient(
-         300deg,
-         rgb(var(--dark)) 12.5%,
-         rgba(var(--cyan), 0.05) 100%
-      );
-      transform: translateY(-0.3rem);
-      .name {
-         margin: 0;
-         font-size: 2.5rem;
-         font-weight: 400;
-         letter-spacing: 1px;
+      height: 38.2rem;
+
+      img {
+         object-position: top;
+         object-fit: cover;
       }
+
+      @media screen {
+         @media (max-width: 915px) {
+            height: 30rem;
+         }
+         @media (max-width: 480px) {
+            min-height: 15rem;
+            height: auto;
+         }
+      }
+   }
+
+   .infos {
+      height: 40%;
+      width: calc(100% - 2rem);
+      min-height: 20rem;
+      padding: 0 1rem 1rem 1rem;
+      background: linear-gradient(
+            30deg,
+            transparent 25%,
+            rgba(var(--cyan), 0.1) 100%
+         ),
+         linear-gradient(200deg, transparent 25%, rgba(var(--cyan), 0.1) 100%);
+      display: grid;
+      grid-template-columns: 20% 80%;
+      grid-template-rows: auto 10% auto 10% 20% 25%;
+      justify-items: end;
+      grid-template-areas:
+         'name name name'
+         'genre genre genre'
+         'publisher-year publisher-year publisher-year'
+         'discount discount discount'
+         'amount amount amount'
+         'wish buy buy';
+      border-radius: 0 0 1rem 1rem;
+      .name {
+         grid-area: name;
+         width: 100%;
+         cursor: pointer;
+         margin: 0;
+         font-size: 2rem;
+         font-weight: 500;
+         letter-spacing: 1px;
+         display: flex;
+         align-items: center;
+         justify-content: flex-start;
+
+         @media screen {
+            @media (max-width: 1150px) {
+               font-size: 2.3rem;
+            }
+            @media (max-width: 1024px) {
+               font-size: 2rem;
+            }
+            @media (max-width: 915px) {
+               font-size: 2.8rem;
+            }
+            @media (max-width: 675px) {
+               font-size: 2.5rem;
+            }
+            @media (max-width: 520px) {
+               font-size: 1.5rem;
+            }
+            @media (max-width: 480px) {
+               margin-top: 0.3rem;
+            }
+         }
+      }
+
       .publisher-year {
-         font-size: 1.2rem;
+         grid-area: publisher-year;
+         width: 100%;
+         font-size: 1.3rem;
          font-weight: 100;
          letter-spacing: 1px;
          color: rgba(var(--purple-2), 0.8);
-         margin-left: 0.3rem;
-      }
-      .genre {
-         font-size: 1.5rem;
-         letter-spacing: 2px;
-         color: rgba(var(--cyan), 0.5);
-         margin-left: 0.3rem;
-      }
-      .price {
-         color: rgba(var(--cyan));
-         margin: 1rem 0;
-         text-align: right;
-         font-size: 3.5rem;
-         font-weight: 600;
-         min-height: 4.8rem;
-         display: flex;
-         flex-direction: column;
-         align-items: flex-end;
-         .discount {
-            font-size: 1.5rem;
-            margin: 0;
-            letter-spacing: 1px;
-         }
 
-         .amount {
-            .code {
-               margin-right: 0.5rem;
+         @media screen {
+            @media (max-width: 1024px) {
+               font-size: 1.2rem;
+            }
+            @media (max-width: 915px) {
                font-size: 1.6rem;
             }
-            .value {
-               letter-spacing: 1px;
+            @media (max-width: 675px) {
+               font-size: 1.3rem;
             }
+            @media (max-width: 520px) {
+               font-size: 1.2rem;
+               letter-spacing: 0;
+            }
+            @media (max-width: 480px) {
+               font-size: 1.1rem;
+            }
+         }
+      }
+
+      .genre {
+         grid-area: genre;
+         width: 100%;
+         font-size: 1.7rem;
+         font-weight: 300;
+         letter-spacing: 2px;
+         color: rgba(var(--cyan), 0.5);
+
+         @media screen {
+            @media (max-width: 1024px) {
+               font-size: 1.5rem;
+            }
+            @media (max-width: 915px) {
+               font-size: 1.8rem;
+            }
+            @media (max-width: 675px) {
+               font-size: 1.5rem;
+            }
+            @media (max-width: 520px) {
+               font-size: 1.2rem;
+            }
+            @media (max-width: 480px) {
+               font-size: 1.1rem;
+            }
+         }
+      }
+
+      .discount {
+         grid-area: discount;
+         font-size: 1.5rem;
+         margin: 0;
+         letter-spacing: 1px;
+         transform: translateY(0.8rem);
+
+         @media screen {
+            @media (max-width: 1024px) {
+               font-size: 1.4rem;
+            }
+            @media (max-width: 915px) {
+               font-size: 1.65rem;
+            }
+            @media (max-width: 675px) {
+               font-size: 1.4rem;
+               transform: translateY(0.5rem);
+            }
+            @media (max-width: 520px) {
+               font-size: 1.3rem;
+            }
+            @media (max-width: 480px) {
+               font-size: 0.9rem;
+               transform: translateY(0.3rem);
+            }
+         }
+      }
+
+      .amount {
+         grid-area: amount;
+         color: rgba(var(--cyan));
+         .code {
+            margin-right: 0.3rem;
+            font-size: 1.6rem;
+            @media screen {
+               @media (max-width: 1024px) {
+                  font-size: 1.5rem;
+               }
+               @media (max-width: 520px) {
+                  font-size: 1.4rem;
+               }
+               @media (max-width: 480px) {
+                  font-size: 1.2rem;
+               }
+            }
+         }
+         .value {
+            font-size: 3.5rem;
+            letter-spacing: 1px;
+            @media screen {
+               @media (max-width: 1024px) {
+                  font-size: 3.2rem;
+               }
+               @media (max-width: 915px) {
+                  font-size: 3.5rem;
+               }
+               @media (max-width: 675px) {
+                  font-size: 3.2rem;
+               }
+               @media (max-width: 520px) {
+                  font-size: 2.5rem;
+               }
+               @media (max-width: 480px) {
+                  font-size: 2rem;
+               }
+            }
+         }
+      }
+
+      .wish-button {
+         grid-area: wish;
+         padding: 0.8rem;
+         border-radius: 50%;
+         margin: 0;
+         width: 4rem;
+         height: 4rem;
+         align-self: flex-end;
+         justify-self: flex-start;
+         svg {
+            width: 100%;
+            height: 100%;
+         }
+
+         @media screen {
+            @media (max-width: 1024px) {
+               width: 3.5rem;
+               height: 3.5rem;
+            }
+            @media (max-width: 915px) {
+               width: 4rem;
+               height: 4rem;
+            }
+            @media (max-width: 520px) {
+               width: 3rem;
+               height: 3rem;
+            }
+            @media (max-width: 390px) {
+               align-self: center;
+            }
+         }
+      }
+
+      .button {
+         grid-area: buy;
+         align-self: flex-end;
+         justify-self: flex-end;
+         .buy-button {
+            min-height: calc(4rem - 2px);
+            font-size: 1.4rem;
+            font-weight: 500;
+            margin: 0;
+            white-space: nowrap;
+            padding: 0 1.5rem;
+
+            @media screen {
+               @media (max-width: 1024px) {
+                  min-height: calc(3.5rem - 2px);
+                  font-size: 1.3rem;
+                  padding: 0 1rem;
+               }
+               @media (max-width: 915px) {
+                  min-height: calc(4rem - 2px);
+                  font-size: 1.4rem;
+                  padding: 0 1.5rem;
+               }
+               @media (max-width: 520px) {
+                  font-size: 0.8rem;
+                  min-height: calc(3rem - 2px);
+               }
+               @media (max-width: 390px) {
+                  padding: 0 1rem;
+               }
+            }
+         }
+
+         @media screen {
+            @media (max-width: 390px) {
+               align-self: center;
+            }
+         }
+      }
+
+      @media screen {
+         @media (max-width: 1130px) {
+            min-height: 23rem;
+         }
+         @media (max-width: 520px) {
+            min-height: 18rem;
+            height: 30%;
+         }
+         @media (max-width: 480px) {
+            width: calc(100% - 1.4rem);
+            min-height: 15rem;
+            padding: 0 0.7rem 0.7rem 0.7rem;
+            height: auto;
+         }
+         @media (max-width: 390px) {
+            min-height: 16rem;
          }
       }
    }
-
-   .buttons {
-      display: flex;
-      align-items: flex-end;
-      justify-content: flex-end;
-      gap: 1rem;
-      .wish-button {
-         font-weight: 500;
-         padding: 0.7rem;
-         border-radius: 50%;
-         margin: 0;
-         white-space: nowrap;
-         justify-content: space-between;
-         width: auto;
-         gap: 0.5rem;
+   @media screen {
+      @media (max-width: 915px) {
+         max-width: 30rem;
+         height: 55rem;
       }
-      .buy-button {
-         font-size: 1.5rem;
-         font-weight: 500;
-         padding: 0.8rem 1rem;
-         margin: 0;
-         white-space: nowrap;
+      @media (max-width: 480px) {
+         min-width: 15rem;
+         height: 35rem;
+         width: 49%;
       }
    }
 `;
--- src/components/VerticalCard/index.jsx
+++ src/components/VerticalCard/index.jsx
@@ -18,60 +18,65 @@ const VerticalCard = ({
    return (
       <>
          <VarticalCardStyles>
-            <Image
-               height={370}
-               width={290}
-               style={{ objectFit: 'cover' }}
-               alt='teste'
-               src={
-                  cover
-                     ? cover
-                     : 'https://www.huber-online.com/daisy_website_files/_processed_/8/0/csm_no-image_d5c4ab1322.jpg'
-               }
-               quality={100}
-               className='cover'
-            />
+            <div className='cover'>
+               <Image
+                  height={600}
+                  width={300}
+                  style={{
+                     height: '100%',
+                     width: '100%',
+                     borderRadius: '1rem 1rem 0 0',
+                  }}
+                  quality={100}
+                  priority
+                  alt={`cover of ${name}`}
+                  src={
+                     cover
+                        ? cover
+                        : 'https://www.huber-online.com/daisy_website_files/_processed_/8/0/csm_no-image_d5c4ab1322.jpg'
+                  }
+               />
+            </div>
             <div className='infos'>
                <h3 className='name'>{name ? name : null}</h3>
                <span className='genre'>{genre ? genre : null}</span>
                <div className='publisher-year'>
                   {publisher ? publisher : null}, {year ? year : null}
                </div>
-               <div className='price'>
-                  {discount ? (
-                     <DiscountPrice
-                        currencyCode={code}
-                        price={((price / 100) * discount).toFixed(2)}
-                        classname={'discount'}
-                     />
-                  ) : (
-                     <div style={{ height: '22px' }}></div>
-                  )}
-                  <div className='amount'>
-                     <span className='code'>{code ? code : 'R$'}</span>
-                     <span className='value'>{price ? price : null}</span>
-                  </div>
-               </div>
-
-               <div className='buttons'>
-                  <Button
-                     icon={<MdFavoriteBorder style={{ width: '2.1rem' }} />}
-                     url={'/'}
-                     className={'wish-button'}
-                     Aboutblank={false}
-                     currentColor={'rgba(var(--purple-2))'}
-                     hoverColor={'rgba(var(--cyan))'}
+               {discount ? (
+                  <DiscountPrice
+                     currencyCode={code}
+                     price={price}
+                     classname={'discount'}
                   />
-                  <ButtonLink
-                     title='Comprar agora'
-                     url={'/'}
-                     className={'buy-button'}
-                     bgColor={'rgba(var(--cyan), 0.1)'}
-                     Aboutblank={false}
-                     currentColor={'rgba(var(--cyan))'}
-                     textTransform={'uppercase'}
-                  />
+               ) : null}
+               <div className='amount'>
+                  <span className='code'>{code ? code : 'R$'}</span>
+                  <span className='value'>
+                     {!discount && price
+                        ? price
+                        : discount
+                        ? ((price / 100) * discount).toFixed(2)
+                        : null}
+                  </span>
                </div>
+               <Button
+                  icon={<MdFavoriteBorder />}
+                  url={'/'}
+                  className={'wish-button'}
+                  Aboutblank={false}
+                  currentColor={'rgba(var(--purple-2))'}
+                  hoverColor={'rgba(var(--cyan))'}
+               />
+               <ButtonLink
+                  title='Comprar agora'
+                  url={'/'}
+                  className={'buy-button'}
+                  bgColor={'rgba(var(--cyan), 0.1)'}
+                  Aboutblank={false}
+                  currentColor={'rgba(var(--cyan))'}
+                  textTransform={'uppercase'}
+               />
             </div>
          </VarticalCardStyles>
       </>
