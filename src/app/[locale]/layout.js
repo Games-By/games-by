e743fe5cce2749f2b7712bf7958d70c2700f4569
Saviovd <savio.pinheiroal@gmail.com>
2024-08-26 15:22:09 -0400

    FIX: next Head error

--- src/app/[locale]/layout.js
+++ src/app/[locale]/layout.js
@@ -2,13 +2,14 @@ import React from 'react';
 import { getMessages } from 'next-intl/server';
 import AppProviders from './providers';
 import GlobalStyle from '@/Styles/globals';
+import Head from 'next/head';
 
 export default async function LocaleLayout({ children, params: { locale } }) {
    const messages = await getMessages();
 
    return (
       <html lang={locale}>
-         <head>
+         <Head>
             <link rel='preconnect' href='https://fonts.googleapis.com' />
             <link
                rel='preconnect'
@@ -19,7 +20,7 @@ export default async function LocaleLayout({ children, params: { locale } }) {
                href='https://fonts.googleapis.com/css2?family=Reddit+Sans:ital,wght@0,200..900;1,200..900&display=swap'
                rel='stylesheet'
             />
-         </head>
+         </Head>
          <body>
             <AppProviders messages={messages}>
                <GlobalStyle />
