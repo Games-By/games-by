4c35ee5e5bb779844733ec685346ca3c1ea08a98
Saviovd <savio.pinheiroal@gmail.com>
2024-08-30 20:48:21 -0400

    UPDATE: recalculating rating

--- src/utils/formatRating.js
+++ src/utils/formatRating.js
@@ -1,23 +1,32 @@
-import { IoIosStar, IoIosStarHalf, IoIosStarOutline } from "react-icons/io";
+import { IoIosStar, IoIosStarHalf, IoIosStarOutline } from 'react-icons/io';
 
 export const getStarIcons = (rating) => {
-   const starCount = (rating / 100) * 5;
+   const starCount = (rating / 10) * 5;
    const fullStars = Math.floor(starCount);
-   const hasHalfStar = starCount % 1 !== 0;
+   const hasHalfStar = starCount - fullStars >= 0.5;
    const emptyStars = 5 - fullStars - (hasHalfStar ? 1 : 0);
 
    const stars = [];
 
    for (let i = 0; i < fullStars; i++) {
-      stars.push(<IoIosStar style={{width: '2rem'}} key={`star-full-${i}`} />);
+      stars.push(
+         <IoIosStar style={{ fontSize: '1.8rem' }} key={`star-full-${i}`} />
+      );
    }
 
    if (hasHalfStar) {
-      stars.push(<IoIosStarHalf style={{width: '2rem'}} key={`star-half`} />);
+      stars.push(
+         <IoIosStarHalf style={{ fontSize: '1.8rem' }} key={`star-half`} />
+      );
    }
 
    for (let i = 0; i < emptyStars; i++) {
-      stars.push(<IoIosStarOutline style={{width: '2rem'}} key={`star-empty-${i}`} />);
+      stars.push(
+         <IoIosStarOutline
+            style={{ fontSize: '1.8rem' }}
+            key={`star-empty-${i}`}
+         />
+      );
    }
 
    return stars;
