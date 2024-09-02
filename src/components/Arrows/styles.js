17d2734be5e5225707773f59c8381637ba5baa30
Saviovd <savio.pinheiroal@gmail.com>
2024-09-02 12:06:34 -0400

    ADD: prev and next buttons

---
+++ src/components/Arrows/styles.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+export const ArrowsContainer = styled.div`
+   display: flex;
+   align-items: center;
+   justify-content: center;
+   gap: 1rem;
+   .arrow {
+      margin: 0;
+      padding: 0;
+      font-size: 2.5rem;
+      border-radius: 50%;
+      width: 4rem;
+      height: 4rem;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+   }
+`;
