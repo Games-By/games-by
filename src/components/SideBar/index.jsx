0d1af99485d52e0d4b2bcec862548d9eacfcbdb7
Saviovd <savio.pinheiroal@gmail.com>
2024-09-06 18:03:22 -0400

    ADD: sidebar content

--- src/components/SideBar/index.jsx
+++ src/components/SideBar/index.jsx
@@ -1,13 +1,37 @@
+import React, { memo } from 'react';
 import useWindowSize from '@/hooks/useWindowSize';
-import { SideBarStyles } from './styles';
+import { GameGenres, SideBarStyles } from './styles';
+import Logo from '../Logo';
+import { Link } from '../../../navigation';
+import Navigation from '../Navigation';
+import { useLocale } from 'next-intl';
+import data from '@/data/menu.json';
+
+const { genresData } = data;
 
 const SideBar = ({ isOpen, setIsOpen }) => {
    const { width } = useWindowSize();
+   const locale = useLocale();
    return (
       <>
-         <SideBarStyles isOpen={isOpen}>teste</SideBarStyles>
+         <SideBarStyles isOpen={isOpen}>
+            <Link href={'/'} className='logo'>
+               <Logo logoWidth={width > 1440 ? 200 : 160} />
+            </Link>
+            <Navigation />
+            <GameGenres>
+               <h2 className='title'>Buscar games por gênero</h2>
+               <ul className='genres'>
+                  {genresData[locale].map((genre, index) => (
+                     <li key={index}>
+                        <Link href={'/'}>{genre}</Link>
+                     </li>
+                  ))}
+               </ul>
+            </GameGenres>
+         </SideBarStyles>
       </>
    );
 };
 
-export default SideBar;
+export default memo(SideBar);
--- src/components/SideBar/styles.js
+++ src/components/SideBar/styles.js
@@ -9,6 +9,23 @@ export const SideBarStyles = styled.div`
    left: 0;
    background-color: rgba(var(--dark));
    transition: left 0.3s ease;
+   display: flex;
+   flex-direction: column;
+
+   .logo {
+      margin: 0 auto;
+      border-bottom: 2px solid rgba(var(--dark-gray));
+      padding: 2rem 0 1.5rem;
+      width: 90%;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      @media screen {
+         @media (max-width: 1440px) {
+            padding: 1rem 0;
+         }
+      }
+   }
 
    @media screen {
       @media (max-width: 1440px) {
@@ -17,6 +34,41 @@ export const SideBarStyles = styled.div`
       @media (max-width: 1024px) {
          left: ${(props) => (props.isOpen ? '0' : '-100%')};
          z-index: 22;
+      }
+   }
+`;
+
+export const GameGenres = styled.div`
+   padding: 2rem 0 1.5rem;
+   width: 90%;
+   margin: 0 auto;
+   border-bottom: 2px solid rgba(var(--dark-gray));
+
+   .title {
+      margin: 0;
+      text-transform: uppercase;
+      font-weight: 500;
+      font-size: 1.4rem;
+   }
+   .genres {
+      padding: 0;
+      display: flex;
+      gap: 1rem 0;
+      flex-wrap: wrap;
+
+      li {
+         width: 50%;
+         text-align: left;
+         color: rgba(var(--light), 0.5);
+         text-transform: capitalize;
+         font-size: 1.1rem;
+         font-weight: 400;
+         transition: 0.4s;
+
+         &:hover {
+            color: rgba(var(--light));
+            font-weight: 500;
+         }
       }
    }
 `;
